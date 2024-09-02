0177ec6acf450036d6fdd54f0f75f86cbe52d757
Chimmz <chimaorji25@gmail.com>
2024-09-02 04:26:22 +0100

    Finished UI for prediction page

--- backend/server.py
+++ backend/server.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # Get model details like ML Model used, score, f1-score, accuracy, precision, recall, etc
 def home():
   return render_template('index.html', me={'name': 'Chima', 'age': 24})
+
+@app.route('/feature-performances')
+def home():
+  return render_template('index.html', me={'name': 'Chima', 'age': 24})
+
+@app.route('/predict', methods=['POST'])
+def home():
+  return render_template('index.html', me={'name': 'Chima', 'age': 24})
+
 
 app.run(debug=True)
